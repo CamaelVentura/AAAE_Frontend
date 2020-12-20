3a9e6b736df814007ac0eb889fb8043c877c44db
Camael Ventura <camael.ventura@gmail.com>
2020-12-19 23:57:58 -0300

    ENV VARIABLES

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import 'dotenv/config';
 
 const api = axios.create({
   headers: { 'X-Requested-With': 'XMLHttpRequest' },
-  baseURL: 'https://aaae-backend.herokuapp.com',
+  baseURL: process.env.URL,
   //baseURL: 'http://localhost:3333',
 });
 
